bae8b9b844271f469054673877c181eb4b4cd848
Richard Quizon <codenomics@proton.me>
2023-05-23 15:29:12 -0700

    added a few notes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
+// should render the number '1' twice. Once in the <h1> tag and the other in the <h4> tag. 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let [ counter, changeCounter ] = useState(1)
+    window.changeCounter = changeCounter
+    return (
+      <div>
+          <h1>{counter}</h1>
+          <h4>{counter}</h4>
+      </div>
+    )
 }
 
 export default App;
+
+// Assignment continues with having us call changeCounter(2) in the developer tool to read the messages generated and to see the number change to 2 on both tags.
